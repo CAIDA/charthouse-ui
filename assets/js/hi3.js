b2d1e4d180ef95e73c5cd3db1b1eaba28e96eeb7
Alistair King <alistair@caida.org>
2018-10-10 13:19:47 -0700

    Add router and a single catch-all route

--- assets/js/hi3.js
+++ assets/js/hi3.js
@@ -7,9 +7,17 @@ import '../css/base.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Explorer from "./Explorer/components/explorer";
+import Login from './auth/login';
 
 ReactDOM.render((
-    <Explorer/>
+    <BrowserRouter>
+        <Switch>
+            <Route path="/">
+                <Explorer/>
+            </Route>
+        </Switch>
+    </BrowserRouter>
 ), document.getElementById('root'));
