093ea68b8742f0c1abfbb6db179042565ec64538
Alistair King <alistair@caida.org>
2018-10-18 16:00:58 -0700

    Start fleshing out auth service

--- assets/js/Hi3/login.jsx
+++ assets/js/Hi3/login.jsx
@@ -1,34 +1,19 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
 
 import { auth } from 'Auth';
 
 class LoginPage extends React.Component {
 
-    state = {
-        isAuthenticated: auth.isAuthenticated
-    };
-
     constructor(props) {
         super(props);
-        this._login = this._login.bind(this);
     }
 
     render() {
-        // no need to log in if we already are
-        if (this.state.isAuthenticated) {
-            // TODO: remember where we were going, otherwise go home
-            return <Redirect to='/explorer'/>;
-        }
-
-        return <div>
-            <p>You need to <a href="javascript:void(0)" onClick={this._login}>log in</a></p>
-        </div>
-    }
-
-    _login() {
-        auth.authenticate();
-        this.setState({isAuthenticated: auth.isAuthenticated});
+        // TODO: here
+        // 1. is this an auth callback?
+        //    yes: handle it and redirect (handle errors)
+        // 2. call the signin method, then render a "logging in..." message (will redirect us somewhere else)
+        return <p>TODO</p>
     }
 }
 
--- assets/js/auth/index.js
+++ assets/js/auth/index.js
@@ -1,18 +1,41 @@
-export {default as AuthenticatedRoute} from './route';
+import { UserManager } from 'oidc-client';
+
+export { default as AuthenticatedRoute } from './route';
 
 class Auth {
     constructor() {
-        this.isAuthenticated = false;
+        const settings = {
+            authority: Constants.stsAuthority,
+            client_id: 'implicit',
+
+            // TODO: should we use the router for this?
+            // TODO: can we redirect to login?
+            redirect_uri: '/login-callback',
+            silent_redirect_uri: `/renew-callback`,
+            post_logout_redirect_uri: '/',
+
+            response_type: 'id_token token',
+            scope: 'openid profile email',
+
+            automaticSilentRenew: true
+        };
+        this.userManager = new UserManager(settings);
     }
 
-    authenticate(cb) {
-        this.isAuthenticated = true;
-        setTimeout(cb, 1000);
+    login() {
+        return this.userManager.signinRedirect();
     }
 
-    signout(cb) {
-        this.isAuthenticated = false;
-        setTimeout(cb, 1000);
+    logout() {
+        return this.userManager.signoutRedirect();
+    }
+
+    renewToken(cb) {
+        return this.userManager.signinSilent();
+    }
+
+    getUser() {
+        return this.userManager.getUser();
     }
 }
 
