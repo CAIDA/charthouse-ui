cedf2f45c9c1aab3b8800c54c98d38b8cba6a432
Alistair King <alistair@caida.org>
2018-10-22 09:37:09 -0400

    Try and get globus auth to work (unsuccessfully)

--- assets/js/auth/index.js
+++ assets/js/auth/index.js
@@ -1,23 +1,26 @@
-import { UserManager } from 'oidc-client';
+import { UserManager, Log } from 'oidc-client';
 
 export { default as AuthenticatedRoute } from './route';
 
 class Auth {
     constructor() {
+        Log.logger = console;
+        Log.level = Log.DEBUG;
+
         const settings = {
-            authority: Constants.stsAuthority,
-            client_id: 'implicit',
+            authority: 'https://auth.globus.org/',
+            client_id: 'fec6a634-d866-4670-a305-54d9eabbd5b0',
 
-            // TODO: should we use the router for this?
-            // TODO: can we redirect to login?
-            redirect_uri: '/login-callback',
-            silent_redirect_uri: `/renew-callback`,
-            post_logout_redirect_uri: '/',
+            // TODO: need to use the router for this to get abs url
+            //redirect_uri: 'https://test.hicube.caida.org/DEBUG',
+            redirect_uri: 'https://auth.globus.org/v2/web/auth-code',
+            //silent_redirect_uri: `https://test.hicube.caida.org/DEBUG`,
+            post_logout_redirect_uri: 'https://test.hicube.caida.org/',
 
-            response_type: 'id_token token',
+            response_type: 'token',
             scope: 'openid profile email',
 
-            automaticSilentRenew: true
+            automaticSilentRenew: false
         };
         this.userManager = new UserManager(settings);
     }
