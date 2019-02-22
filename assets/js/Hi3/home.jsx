10de2a9f4737c1aa5d1f9e549a3be533279054e0
Alistair King <alistair@caida.org>
2019-02-22 13:48:39 -0800

    Add some placeholder tiles to home page

--- assets/js/Hi3/home.jsx
+++ assets/js/Hi3/home.jsx
@@ -2,10 +2,83 @@ import React from 'react';
 
 import caidaLogo from 'images/logos/caida-logo-cropped.svg';
 import hi3Logo from 'images/logos/hicube-full.png';
+import explorerThumb from 'images/logos/hicube-icon.png';
+import dashboardThumb from 'images/logos/hicube-icon.png';
+import examplesThumb from 'images/logos/hicube-icon.png';
 
 import 'Hi3/css/home.css';
 
-import LinkButton from 'utils/link-button';
+class InterfaceTiles extends React.Component {
+    render() {
+        return <div>
+            <h3>Data Interfaces</h3>
+        <div className="row row-thumbs">
+            <a href="/explorer">
+                <div className="col-md-4">
+                    <div className="thumbnail">
+                        <img src={explorerThumb}/>
+                        <div className="caption text-center">
+                            <h4>
+                                Time Series Explorer
+                            </h4>
+                            <p>
+                                Interface for exploratory analysis of Internet security time
+                                series. Allows researchers to
+                                build custom visualizations by composing time
+                                series
+                                together using specialized post-processing
+                                functions.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a href="/dashboards">
+                <div className="col-md-4">
+                    <div className="thumbnail">
+                        <img src={dashboardThumb}/>
+                        <div className="caption text-center">
+                            <h4>
+                                Live Dashboards
+                            </h4>
+                            <p>
+                                Interface for exploratory analysis of Internet
+                                security time
+                                series. Allows researchers to
+                                build custom visualizations by composing time
+                                series
+                                together using specialized post-processing
+                                functions.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a href="/examples">
+                <div className="col-md-4">
+                    <div className="thumbnail">
+                        <img src={examplesThumb}/>
+                        <div className="caption text-center">
+                            <h4>
+                                Sample Analyses
+                            </h4>
+                            <p>
+                                Interface for exploratory analysis of Internet
+                                security time
+                                series. Allows researchers to
+                                build custom visualizations by composing time
+                                series
+                                together using specialized post-processing
+                                functions.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        </div>;
+    }
+}
 
 class Home extends React.Component {
     render() {
@@ -28,6 +101,7 @@ class Home extends React.Component {
                     </i>
                 </p>
             </div>
+            <InterfaceTiles/>
         </div>;
     }
 }
