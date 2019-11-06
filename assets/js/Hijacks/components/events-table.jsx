d25c4cde3cce5e7228080330162511abd964e7e9
Mingwei Zhang <mingwei@caida.org>
2019-11-06 13:51:29 -0800

    port of time-picker

--- assets/js/Hijacks/components/events-table.jsx
+++ assets/js/Hijacks/components/events-table.jsx
@@ -1,3 +1,12 @@
+import DateRangePicker from 'react-bootstrap-daterangepicker';
+import moment from 'moment'
+// you will need the css that comes with bootstrap@3. if you are using
+// a tool like webpack, you can do the following:
+import 'bootstrap/dist/css/bootstrap.css';
+// you will also need the css that comes with bootstrap-daterangepicker
+import 'bootstrap-daterangepicker/daterangepicker.css';
+
+import 'Hijacks/css/hijacks.css';
 import PropTypes from 'prop-types';
 import React from 'react';
 import DataTable from 'react-data-table-component';
@@ -11,13 +20,7 @@ function unix_time_to_str(unix_time) {
     if (unix_time === null) {
         return ""
     }
-    let d = new Date(unix_time * 1000);
-    let year = d.getUTCFullYear();
-    let month = zeroPad(d.getUTCMonth(), 2);
-    let day = zeroPad(d.getUTCDate(), 2);
-    let hour = zeroPad(d.getUTCHours(), 2);
-    let minute = zeroPad(d.getUTCMinutes(), 2);
-    return `${year}-${month}-${day} ${hour}:${minute}`;
+    return moment(unix_time * 1000).format("YYYY-MM-DD HH:mm");
 }
 
 const columns = [
@@ -108,81 +111,100 @@ class EventsTable extends React.Component {
         totalRows: 0,
         perPage: 10,
         curPage: 0,
+        startTime: 0,
+        endTime: 0,
+        timeRangeStr: ""
     };
 
-    async componentDidMount() {
-        const {perPage} = this.state;
+    constructor(props) {
+        super(props);
 
-        this.setState({loading: true});
+        this.ranges = {
+            'Today': [
+                moment().startOf('day').utc(true),
+                moment().utc(true)
+            ],
+            'Yesterday': [
+                moment().subtract(2, 'days').startOf('day').utc(true),
+                moment().subtract(1, 'days').startOf('day').utc(true)
+            ],
+            'Last 7 Days': [
+                moment().subtract(6, 'days').startOf('day').utc(true),
+                moment().utc(true)
+            ],
+            'Last 30 Days': [
+                moment().subtract(29, 'days').startOf('day').utc(true),
+                moment().utc(true)
+            ],
+            'This Month': [
+                moment().startOf('month').utc(true),
+                moment().endOf('month').utc(true)
+            ],
+            'Last Month': [
+                moment().subtract(1, 'month').startOf('month').utc(true),
+                moment().subtract(1, 'month').endOf('month').utc(true)
+            ]
+        };
 
-        const response = await axios.get(
-            `https://bgp.caida.org/json/events/${this.props.eventType}?length=10`,
-        );
-        console.log(response);
+        this.state.startTime = moment().startOf('day').utc(true);
+        this.state.endTime = moment().utc(true);
 
-        this.setState({
-            data: response.data.data,
-            totalRows: response.data.recordsTotal,
-            loading: false,
-        });
+        this.state.timeRangeStr = `${this.state.startTime.format()} - ${this.state.endTime.format()}`;
+
+        this.handleDateRangeChange = this.handleDateRangeChange.bind(this);
+        this.loadContent = this.loadContent.bind(this);
+
     }
 
-    async componentDidUpdate(prevProps) {
+    componentDidMount() {
+        this.loadContent();
+    }
+
+    componentDidUpdate(prevProps) {
         if (prevProps.eventType === this.props.eventType) {
             return
         }
-        const {perPage, curPage} = this.state;
-        let page = curPage - 1;
+        this.loadContent()
+    }
 
-        this.setState({loading: true});
+    async loadContent(
+        perPage = this.state.perPage,
+        curPage = this.state.curPage,
+        ts_start = this.state.startTime,
+        ts_end = this.state.endTime,
+    ) {
+        this.setState({
+            loading: true,
+            curPage: curPage,
+            perPage: perPage,
+        });
 
-        const response = await axios.get(
-            `https://bgp.caida.org/json/events/${this.props.eventType}?length=${perPage}&start=${perPage * page}`,
-        );
+        let url = `https://bgp.caida.org/json/events/${this.props.eventType}?length=${perPage}&start=${perPage * curPage}` +
+            `&ts_start=${ts_start.format("YYYY-MM-DDTHH:mm")}` +
+            `&ts_end=${ts_end.format("YYYY-MM-DDTHH:mm")}`;
+        console.log(url);
+
+        const response = await axios.get(url);
         console.log(response);
 
         this.setState({
             data: response.data.data,
             totalRows: response.data.recordsTotal,
+            curPage: curPage,
+            perPage: perPage,
             loading: false,
         });
     }
 
     handlePageChange = async page => {
-        page -= 1;
-        console.log("page changed to %s", page);
-        const {perPage} = this.state;
-        this.state.curPage = page;
-
-        this.setState({loading: true});
-
-        const response = await axios.get(
-            `https://bgp.caida.org/json/events/${this.props.eventType}?length=${perPage}&start=${perPage * page}`,
-        );
-
-        this.setState({
-            loading: false,
-            data: response.data.data,
-        });
+        this.state.curPage = page - 1;
+        this.loadContent();
     };
 
     handlePerRowsChange = async (perPage, page) => {
-        page -= 1;
-        this.setState({
-            loading: true,
-            perPage: perPage,
-            curPage: page,
-        });
-
-        const response = await axios.get(
-            `https://bgp.caida.org/json/events/${this.props.eventType}?length=${perPage}&start=${perPage * page}`,
-        );
-
-        this.setState({
-            loading: false,
-            data: response.data.data,
-            perPage,
-        });
+        this.state.perPage = perPage;
+        this.state.curPage = page - 1;
+        this.loadContent(perPage, page);
     };
 
     handleRowClick(row) {
@@ -190,22 +212,55 @@ class EventsTable extends React.Component {
         window.open(`/feeds/hijacks/events/${row.event_type}/${row.id}`, "_self");
     }
 
+    handleDateRangeChange(event, picker) {
+        this.setState({
+            startTime: picker.startDate.utc(),
+            endTime: picker.endDate.utc(),
+            timeRangeStr: `${picker.startDate.utc(true).format()} - ${picker.endDate.utc(true).format()}`,
+        });
+        this.loadContent()
+    }
+
     render() {
-        const {loading, data, totalRows} = this.state;
 
         return (
-            <DataTable
-                title="Events List"
-                columns={columns}
-                data={data}
-                progressPending={loading}
-                pagination
-                paginationServer
-                paginationTotalRows={totalRows}
-                onChangeRowsPerPage={this.handlePerRowsChange}
-                onChangePage={this.handlePageChange}
-                onRowClicked={this.handleRowClick}
-            />
+            <div className={"row"}>
+                <div className={"event-list__time"}>
+                    <DateRangePicker
+                        startDate={this.state.startTime}
+                        endDate={this.state.endTime}
+                        onApply={this.handleDateRangeChange}
+                        ranges={this.ranges}
+                        alwaysShowCalendars={true}
+                        timePicker={true}
+                        timePicker24Hour={true}
+                        timePickerIncrement={15}
+                    >
+                        <a href="#" className="btn btn-info btn-md">
+                            <span className="glyphicon glyphicon-calendar"/> Time Range
+                        </a>
+                    </DateRangePicker>
+
+                    <input
+                        readOnly={true}
+                        className={"form-control event-list__time-input"}
+                        value={this.state.timeRangeStr}
+                    />
+                </div>
+
+                <DataTable
+                    title="Events List"
+                    columns={columns}
+                    data={this.state.data}
+                    progressPending={this.state.loading}
+                    pagination
+                    paginationServer
+                    paginationTotalRows={this.state.totalRows}
+                    onChangeRowsPerPage={this.handlePerRowsChange}
+                    onChangePage={this.handlePageChange}
+                    onRowClicked={this.handleRowClick}
+                />
+            </div>
         );
     }
 }
